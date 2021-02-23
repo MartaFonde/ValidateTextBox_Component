8acd99289599e2640ccc3c237088920a0164f564
martafonde <marta4510@hotmail.com>
2021-02-24 00:21:33 +0100

    Agregar archivos de proyecto.

---
+++ TestForm/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.validateTextBox1 = new ValidateTextBoxComponent.ValidateTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // validateTextBox1
+            // 
+            this.validateTextBox1.Location = new System.Drawing.Point(70, 33);
+            this.validateTextBox1.Multilinea = true;
+            this.validateTextBox1.Name = "validateTextBox1";
+            this.validateTextBox1.Size = new System.Drawing.Size(477, 40);
+            this.validateTextBox1.TabIndex = 0;
+            this.validateTextBox1.Texto = "";
+            this.validateTextBox1.Tipo = ValidateTextBoxComponent.eTipo.Numerico;
+            this.validateTextBox1.TxtChanged += new System.EventHandler(this.validateTextBox1_TxtChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.validateTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ValidateTextBoxComponent.ValidateTextBox validateTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ValidateTextBoxComponent/ValidateTextBox.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidateTextBoxComponent
+{
+    public enum eTipo
+    {
+        Numerico,
+        Textual
+    }
+
+    [
+        DefaultProperty("Tipo"),
+        DefaultEvent("TxtChanged")
+    ]
+    public partial class ValidateTextBox : UserControl
+    {
+        bool colorRojo = true;
+
+        private eTipo tipo;
+
+        [Category("Design")]
+        [Description("Tipo de caracteres del texto del TextBox")]
+        public eTipo Tipo{
+            set
+            {
+                if(Enum.IsDefined(typeof(eTipo), value))
+                {
+                    tipo = value;
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return tipo;
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Texto asociado al TextBox del control")]
+        public String Texto
+        {
+            set
+            {
+                txt.Text = value;                
+
+                if (Tipo == eTipo.Numerico)
+                {
+                    try
+                    {
+                        colorRojo = false;
+                        Convert.ToInt32(txt.Text.Trim());
+                    }
+                    catch (FormatException)
+                    {
+                        colorRojo = true;
+                    }
+                }
+                else
+                {
+                    string texto = txt.Text.ToLower();
+                    colorRojo = false;
+                    for (int i = 0; i < texto.Length; i++)
+                    {
+                        if (texto[i] < 'a' || texto[i] > 'z' && texto[i] != ' ')
+                        {
+                            colorRojo = true;
+                        }
+                    }
+                }
+                Refresh();
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+        [Category("Behavior")]
+        [Description("Controla si el texto del TextBox puede ocupar más de una línea")]
+        public bool Multilinea
+        {
+            set
+            {
+                txt.Multiline = value;
+            }
+            get
+            {
+                return txt.Multiline;
+            }
+        }
+
+        [Category("El texto de Textbox cambió")]
+        [Description("Se lanza cuando el texto de la Textbox cambia")]
+        public event System.EventHandler TxtChanged;
+
+        public ValidateTextBox()
+        {
+            InitializeComponent();
+            tipo = eTipo.Numerico;
+            colorRojo = true;
+        }
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+            Texto = txt.Text;
+            this.TxtChanged?.Invoke(sender, e);          //Acceso ao evento desde o compoñente  
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            txt.Location = new Point(10, 10);
+            this.Height = txt.Height + 20;
+            txt.Width = this.Width - 20;
+
+            Pen p = new Pen(colorRojo ? Color.Red : Color.Green);
+            p.Width = 3;
+            e.Graphics.DrawRectangle(p, new Rectangle(5, 5, this.Width - 5, this.Height - 5));
+
+            e.Dispose();
+        }
+    }
+}
